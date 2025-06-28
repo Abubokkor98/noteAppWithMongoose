ec2c4fb726ab7f0b009987237ff9de3ba5485ffb
Abubokkor98 <mail.abubokkor@gmail.com>
2025-06-28 14:37:23 +0600

    17-4 Understanding Data Types and Required Fields in Schema

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,24 @@ const app: Application = express();
 
 //schemas
 const noteSchema = new Schema({
-  title: String,
-  content: String,
+  title: {
+    type: String,
+    required: true,
+    trim: true, //  "    hello word   " //trim white space remove korbe
+  },
+  content: {
+    type: String,
+    default: "",
+  },
+  category: {
+    type: String,
+    enum: ["personal", "work", "study"],
+    default: "personal",
+  },
+  pinned: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 //model
@@ -15,7 +31,7 @@ const Note = model("Note", noteSchema);
 app.post("/create-note", async (req: Request, res: Response) => {
   const newNote = new Note({
     title: "Learning Mongoose",
-    content: "I am learning mongoose",
+    // content: "I am learning mongoose",
   });
 
   await newNote.save();
@@ -28,10 +44,10 @@ app.post("/create-note", async (req: Request, res: Response) => {
 
 app.get("/notes", async (req: Request, res: Response) => {
   const notes = await Note.find();
-    res.status(200).json({
-      success: true,
-      data: notes,
-    });
+  res.status(200).json({
+    success: true,
+    data: notes,
+  });
 });
 
 app.get("/", (req: Request, res: Response) => {
