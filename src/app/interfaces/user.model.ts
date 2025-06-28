1daef850953e6bed18f3a04634e375153808830c
Abubokkor98 <mail.abubokkor@gmail.com>
2025-06-28 19:24:03 +0600

    17-8 Integrating Typescript with Mongoose And Making User Model

---
+++ src/app/interfaces/user.model.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "./user.interfaces";
+
+const userSchema = new Schema<IUser>({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
+
+const User = model("User", userSchema);
