83227cff2dae2c07b58f6403e9f1854e53056900
Abubokkor98 <mail.abubokkor@gmail.com>
2025-06-29 12:16:28 +0600

    18-1 Validations in Mongoose: Built-in and Custom validations [max/min]

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,20 +2,30 @@ import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interfaces";
 
 const userSchema = new Schema<IUser>({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
   firstName: {
     type: String,
     required: true,
     trim: true,
+    minlength: [3, "First name must be at least 3 characters"], //custom error message
+    maxlength: 15,
   },
   lastName: {
     type: String,
     required: true,
     trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    min: [18, "Age must be at least 18"],
+    max: 120,
+    required: true,
   },
   password: {
     type: String,
